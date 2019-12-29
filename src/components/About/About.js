f4ab7f0aa33e791c92115f19c62ec6114d406863
lee <iop2480@naver.com>
2019-12-29 17:59:25 +0900

    change 자기소개

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -32,6 +32,10 @@ const Container = styled.div`
         margin-top: 2px;
     }
 
+    & p:nth-child(3) {
+        margin-top: 40px;
+    }
+
     transition: visibility 0s, opacity 0.5s linear;
     @media screen and (max-width: 767px){
         display:  ${props => props.isDisplayed ? 'none' : 'block'};
@@ -62,12 +66,14 @@ class About extends Component {
                     </div>
                     <div>
                         <p>안녕하세요 웹 개발자 이승현입니다.</p>
-                        <p>2015년부터 직장생활을 하고 있고, 현재 성남에서 거주하고 있습니다.</p>
-                        <p>
-                              
-                        </p>
-                        <p>
-                            
+                        <p>2015년부터 직장생활을 하고 있고, 현재 성남에서 거주하고 있습니다.
+                        Front-end(웹 퍼블리싱 포함) , back-end 둘 다 개발을 해왔었고, Grid, Chart 라이브러리들을 활용해
+                        Front-end쪽 개발을 주로 해 왔습니다.  js와 Front-end쪽에 관심이 많습니다.</p>
+
+                        <p>Hi. this is Seunghyun</p>
+                        <p>I've been working as programmer since 2015, now I've lived in Seongnam, in Korea.
+                           I've developed both Front-end, Back-end and mainly developed Front-end part using Grid, Chart Library
+                           I've interested in js, Front-end part
                         </p>
                     </div>
                 </Container>
